9cc71b7f92f5737d341acbac3149d2c737875e75
ddkan8 <ddkan8@qq.com>
2016-01-15 03:26:38 +0800

    gulp

--- miaov/gulp_web/gulpfile.js
+++ miaov/gulp_web/gulpfile.js
@@ -1,40 +1,85 @@
 // 引入gulp和插件
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
+    sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
+    jade = require('gulp-jade'),
     rename = require('gulp-rename'),
     clean = require('gulp-clean'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
+    del = require('del'),
     livereload = require('gulp-livereload');
 
-// 编译Sass，Autoprefix及压缩css
-gulp.task('styles', function() {  
-  return gulp.src('src/styles/main.scss')
-    .pipe(sass({ style: 'expanded' }))
+// Styles任务
+gulp.task('styles', function() {
+    //编译sass
+    return gulp.src('src/styles/main.scss')
+    .pipe(sass())
+    //添加前缀
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    //保存未压缩文件到我们指定的目录下面
     .pipe(gulp.dest('dist/assets/css'))
-    .pipe(rename({suffix: '.min'}))
+    //给文件添加.min后缀
+    .pipe(rename({ suffix: '.min' }))
+    //压缩样式文件
     .pipe(minifycss())
+    //输出压缩文件到指定目录
     .pipe(gulp.dest('dist/assets/css'))
+    //提醒任务完成
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
-// JSHint，拼接及压缩JavaScript
-gulp.task('scripts', function() {  
+// // Styles任务
+// gulp.task('styles', function() {
+//   //编译sass
+//   return gulp.src('src/styles/main.scss')
+//     .pipe(sass({ style: 'expanded' }))
+//     //添加前缀
+//     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+//     //保存未压缩文件到我们指定的目录下面
+//     .pipe(gulp.dest('dist/assets/css'))
+//     //给文件添加.min后缀
+//     .pipe(rename({suffix: '.min'}))
+//     //压缩样式文件
+//     .pipe(minifycss())
+//     //输出压缩文件到指定目录
+//     .pipe(gulp.dest('dist/assets/css'))
+//     //提醒任务完成
+//     .pipe(notify({ message: 'Styles task complete' }));
+// });
+
+// gulp.task('styles', function() {
+//   return sass('src/styles/main.scss', { style: 'expanded' })
+//     .pipe(autoprefixer('last 2 version'))
+//     .pipe(gulp.dest('dist/assets/css'))
+//     .pipe(rename({suffix: '.min'}))
+//     .pipe(minifycss())
+//     .pipe(gulp.dest('dist/assets/css'))
+//     .pipe(notify({ message: 'Styles task complete' }));
+// });
+
+// Scripts任务
+gulp.task('scripts', function() {
+  //js代码校验
   return gulp.src('src/scripts/**/*.js')
-    .pipe(jshint('.jshintrc'))
+    //.pipe(jshint('.jshintrc'))
+    .pipe(jshint())
     .pipe(jshint.reporter('default'))
+    //js代码合并
     .pipe(concat('main.js'))
     .pipe(gulp.dest('dist/assets/js'))
+    //给文件添加.min后缀
     .pipe(rename({suffix: '.min'}))
+    //压缩脚本文件
     .pipe(uglify())
+    //输出压缩文件到指定目录
     .pipe(gulp.dest('dist/assets/js'))
+    //提醒任务完成
     .pipe(notify({ message: 'Scripts task complete' }));
 });
 
@@ -42,19 +87,35 @@ gulp.task('scripts', function() {
 gulp.task('images', function() {  
   return gulp.src('src/images/**/*')
     .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    //.pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
     .pipe(gulp.dest('dist/assets/img'))
     .pipe(notify({ message: 'Images task complete' }));
 });
 
-// 清除文件
-gulp.task('clean', function() {  
-  return gulp.src(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], {read: false})
-    .pipe(clean());
+// 编译Jade
+gulp.task('templates', function() {
+  var YOUR_LOCALS = {};
+ 
+  gulp.src('./views/**/*.jade')
+    .pipe(jade({
+      locals: YOUR_LOCALS
+    }))
+    .pipe(gulp.dest('./dist/assets/html'))
+    .pipe(notify("Templates task complete"))
 });
 
+// 清除文件
+// gulp.task('clean', function() {  
+//   return gulp.src(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], {read: false})
+//     .pipe(clean());
+// });
+gulp.task('clean',function(cb){
+  del(['./dist'],cb)
+})
+
 // 默认任务
-gulp.task('default', ['clean'], function() {  
-    gulp.start('styles', 'scripts', 'images');
+gulp.task('default', ['clean', 'watch'], function() {  
+    gulp.start('styles', 'scripts', 'images', 'templates');
 });
 
 // 监听文件变化
@@ -69,16 +130,22 @@ gulp.task('watch', function() {
   // 监听所有图片档
   gulp.watch('src/images/**/*', ['images']);
 
-});
+  gulp.watch('views/*.jade',['templates']);
 
-gulp.task('watch', function() {
+  livereload.listen();
 
-  // 建立实时刷新服务器
-  var server = livereload();
-
-  // 监听所有在 dist/  目录下的文件，一旦有更新，便进行刷新
-  gulp.watch(['dist/**']).on('change', function(file) {
-    server.changed(file.path);
-  });
+  gulp.watch(['dist/**']).on('change', livereload.changed);
 
 });
+
+// gulp.task('watch', function() {
+
+//   // 建立实时刷新服务器
+//   var server = livereload();
+
+//   // 监听所有在 dist/  目录下的文件，一旦有更新，便进行刷新
+//   gulp.watch(['dist/**']).on('change', function(file) {
+//     server.changed(file.path);
+//   });
+
+// });
